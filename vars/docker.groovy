0313ada51c1d85d4b27506b096f8348b719f814a
SriChandana24 <srichandana246@gmail.com>
2022-08-08 15:55:57 +0530

    added variable type

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,4 +1,4 @@
-def dockerize(image){
+def dockerize(String image){
   sh 'sudo service docker start'    
   sh 'sudo docker login -u="srichandana" -p="docker@2022"'        
   sh 'sudo docker build --tag srichandana/${image}:${BUILD_NUMBER} .'
