5358872cb2df9eb1e5b077094bef0545f900a1d7
SriChandana24 <srichandana246@gmail.com>
2022-08-08 16:16:31 +0530

    modified image param

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,6 +1,6 @@
-def dockerize(String image){
+def dockerize(String imageName){
   sh 'sudo service docker start'    
   sh 'sudo docker login -u="srichandana" -p="docker@2022"'        
-  sh 'sudo docker build --tag srichandana/${image}:${BUILD_NUMBER} .'
-  sh 'sudo docker push srichandana/${image}:${BUILD_NUMBER}' 
+  sh 'sudo docker build --tag srichandana/${imageName}:${BUILD_NUMBER} .'
+  sh 'sudo docker push srichandana/${imageName}:${BUILD_NUMBER}' 
 }
