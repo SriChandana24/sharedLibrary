50f32d88cc116199ff6b3087f85fbc2ea807bbe1
SriChandana24 <srichandana246@gmail.com>
2022-08-11 16:25:01 +0530

    added helm file

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,3 +1,4 @@
-def helm_upgrade(){
+def call(){
+  sh 'aws eks update-kubeconfig --region ${AWS_REGION} --name ${AWS_EKS_CLUSTER}'
   sh 'helm upgrade --install ${HELMCHART_NAME} --set image.tag=${BUILD_NUMBER} ./helm/${HELMCHART_NAME}'
 }
